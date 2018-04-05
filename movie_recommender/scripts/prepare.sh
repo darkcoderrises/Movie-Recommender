1797a03f5679da0eb5f8e4b70658416f54b8063c
Aditi Gangrade <aditigangrade200@gmail.com>
2018-04-05 12:21:46 +0530

    updated seed script

--- movie_recommender/scripts/prepare.sh
+++ movie_recommender/scripts/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python manage.py mlens --path seed/movielens
-python manage.py seed --path seed/location.csv,seed/theater.csv
+python manage.py seed --path seed/location.csv,seed/theatre_location.csv
 python manage.py rebuild_index
 python manage.py build_similar
